c496e29bebe8e376844bfac191cac3f317a06111
abigailnm111 <60757439+abigailnm111@users.noreply.github.com>
2021-05-31 13:21:07 -0700

    added iris dataset

--- IrisClassification.py
+++ IrisClassification.py
@@ -21,3 +21,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+
+#load dataset
+url='iris.csv'
+names= ['sepal-length', 'sepal-width', 'petal-length','petal-width','class']
+dataset=read_csv(url, names=names)
+print(dataset.shape)
