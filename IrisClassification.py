02cbd285653940206d511511b0511e8ca5c8bf86
abigailnm111 <60757439+abigailnm111@users.noreply.github.com>
2021-06-01 21:57:35 -0700

    ran various models

--- IrisClassification.py
+++ IrisClassification.py
@@ -26,4 +26,39 @@ from sklearn.svm import SVC
 url='iris.csv'
 names= ['sepal-length', 'sepal-width', 'petal-length','petal-width','class']
 dataset=read_csv(url, names=names)
-print(dataset.shape)
+#print(dataset.shape)
+#print(dataset.head(20))
+#print(dataset.describe())
+#print(dataset.groupby('class').size())
+#dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+#dataset.hist()
+#scatter_matrix(dataset)
+#pyplot.show()
+
+#set aside validation dataset
+array=dataset.values
+X=array[:,0:4]
+Y=array[:,4]
+X_train, X_validation, Y_train, Y_validation= train_test_split(X,Y, test_size=0.2, random_state=1)
+
+
+models=[]
+models.append(("LR", LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(("LDA", LinearDiscriminantAnalysis()))
+models.append(("KNN", KNeighborsClassifier()))
+models.append(("CART", DecisionTreeClassifier()))
+models.append(("NB", GaussianNB()))
+models.append(("SVM", SVC(gamma='auto')))
+
+results=[]
+names=[]
+for name, model in models:
+    kfold= StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results=cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s:%f(%f)'%(name, cv_results.mean(), cv_results.std()))
+    
+pyplot.boxplot(results, labels=names)
+pyplot.title("Algorthm Comparison")
+pyplot.show()
